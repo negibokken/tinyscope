f845e4ff04ebf35f2aca816e9ff772c07ab28719
bokken <negibokken@gmail.com>
2021-11-25 22:51:40 +0900

    Add simple new method and initial state for tokenizer

--- src/html/tokenizer.rs
+++ src/html/tokenizer.rs
@@ -2,3 +2,27 @@
 pub struct Tokenizer {
     state: State,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum State {
+    DataState,
+}
+
+impl Tokenizer {
+    pub fn new() -> Self {
+        Tokenizer {
+            state: State::DataState,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn initial_state_is_data() {
+        let tokenizer = Tokenizer::new();
+        assert_eq!(tokenizer.state, State::DataState);
+    }
+}
