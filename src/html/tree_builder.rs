f91185be7f50a44fa0d2fb18501bde61517ac042
bokken <negibokken@gmail.com>
2021-11-29 18:05:37 +0900

    Add tree_builder::State

--- src/html/tree_builder.rs
+++ src/html/tree_builder.rs
@@ -1,8 +1,28 @@
 #[derive(Debug, Clone, Copy)]
-pub struct TreeBuilder {}
+pub struct TreeBuilder {
+    state: State,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum State {
+    Initial,
+}
 
 impl TreeBuilder {
     pub fn new() -> TreeBuilder {
-        TreeBuilder {}
+        TreeBuilder {
+            state: State::Initial,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn initial_state_is_initial() {
+        let tree_builder = TreeBuilder::new();
+        assert_eq!(tree_builder.state, State::Initial);
     }
 }
